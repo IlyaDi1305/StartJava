091c38362d9013f52e701f3a4c2d45e1142ae170
IlyaDi1305 <didorenko1305@gmail.com>
2022-12-25 22:33:04 +0400

    correction 24.12.22

--- src/com/startjava/Lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/Lesson_2_3_4/calculator/Calculator.java
@@ -1,31 +1,12 @@
 package com.startjava.Lesson_2_3_4.calculator;
 
 public class Calculator {
-    private int number1;
-    private int number2;
-    private char mathSign;
 
-    public int getNumber1() {
-        return number1;
-    }
-
-    public int getNumber2() {
-        return number2;
-    }
-
-    public char getMathSign() {
-        return mathSign;
-    }
-
-    public void setExpression(String expression) {
+    public static int calculate(String expression) {
         String[] partsExpression = expression.split(" ");
-        number1 = Integer.parseInt(partsExpression[0]);
-        mathSign = partsExpression[1].charAt(0);
-        number2 = Integer.parseInt(partsExpression[2]);
-    }
-
-    public double calculate() {
-        switch (mathSign) {
+        int number1 = Integer.parseInt(partsExpression[0]);
+        int number2 = Integer.parseInt(partsExpression[2]);
+        switch (partsExpression[1].charAt(0)) {
             case '+':
                 return Math.addExact(number1, number2);
             case '-':
@@ -35,11 +16,10 @@ public class Calculator {
             case '%':
                 return Math.floorMod(number1, number2);
             case '/':
-                return (double) number1 / number2;
+                return number1 / number2;
             case '^':
-                return Math.pow(number1, number2);
-        }
-        return 0;
+                return (int) Math.pow(number1, number2);
+        }return 0;
     }
 }
 
--- src/com/startjava/Lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/Lesson_2_3_4/calculator/CalculatorTest.java
@@ -4,15 +4,12 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] agre) {
-        Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
         String userResponse = "да";
         while (!userResponse.equals("нет")) {
             if (userResponse.equals("да")) {
                 System.out.print("Введите математическое выражение: ");
-                String enteredExpression = scan.nextLine();
-                calc.setExpression(enteredExpression);
-                System.out.println(enteredExpression + " = " + calc.calculate());
+                System.out.println(Calculator.calculate(scan.nextLine()));
             }
             System.out.print("Хотите продолжить вычисления? [да/нет]: ");
             userResponse = scan.nextLine();
