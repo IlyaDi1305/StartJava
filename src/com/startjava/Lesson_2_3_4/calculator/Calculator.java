97f2d27a271abcddd53674fd78b64b6c0bcf2047
IlyaDi1305 <didorenko1305@gmail.com>
2022-12-26 20:14:24 +0400

    corrected 26.12.22

--- src/com/startjava/Lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/Lesson_2_3_4/calculator/Calculator.java
@@ -4,22 +4,29 @@ public class Calculator {
 
     public static int calculate(String expression) {
         String[] partsExpression = expression.split(" ");
-        int number1 = Integer.parseInt(partsExpression[0]);
-        int number2 = Integer.parseInt(partsExpression[2]);
-        switch (partsExpression[1].charAt(0)) {
-            case '+':
-                return Math.addExact(number1, number2);
-            case '-':
-                return Math.subtractExact(number1, number2);
-            case '*':
-                return Math.multiplyExact(number1, number2);
-            case '%':
-                return Math.floorMod(number1, number2);
-            case '/':
-                return number1 / number2;
-            case '^':
-                return (int) Math.pow(number1, number2);
-        }return 0;
+        try {
+            int number1 = Integer.parseInt(partsExpression[0]);
+            int number2 = Integer.parseInt(partsExpression[2]);
+                if (number1 < 0 || number2 < 0) {
+                    System.out.println("Числа не могут быть отрицательными");
+                    return 0;
+            }
+            return switch (partsExpression[1].charAt(0)) {
+                case '+' -> Math.addExact(number1, number2);
+                case '-' -> Math.subtractExact(number1, number2);
+                case '*' -> Math.multiplyExact(number1, number2);
+                case '%' -> Math.floorMod(number1, number2);
+                case '/' -> number1 / number2;
+                case '^' -> (int) Math.pow(number1, number2);
+                default -> {
+                    System.out.println("Введите корректный знак операции");
+                    yield 0;
+                }
+            };
+        } catch (Exception e) {
+            System.out.println("Ошибка ввода данных");
+            return 0;
+        }
     }
 }
 
--- src/com/startjava/Lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/Lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package com.startjava.Lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
+
     public static void main(String[] agre) {
         Scanner scan = new Scanner(System.in);
         String userResponse = "да";
